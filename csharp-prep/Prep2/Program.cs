9afe39af842f5657770784d985a8963f8d6d065f
Yoav-Galindo <gal22005@byui.edu>
2024-02-28 06:52:18 -0700

    prep2task

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string sPercentage = Console.ReadLine();
+        int iPercentage = int.Parse(sPercentage);
+
+        string letter = "";
+        
+        if(iPercentage >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (iPercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (iPercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (iPercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got the grade {letter}");
+        
+        Console.WriteLine();
+        if(iPercentage >= 70)
+        {
+            Console.WriteLine("You passed the class, congratulations!!!");
+        }
+        else
+        {
+            Console.WriteLine("You fail the class, good luck next time.");
+        }
     }
 }
