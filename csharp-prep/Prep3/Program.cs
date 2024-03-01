af854ca4064fce7047c361b0c89ea0536930d587
Yoav-Galindo <gal22005@byui.edu>
2024-02-29 21:19:10 -0700

    guessNumberGame

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine();
+        Console.WriteLine("**************** WECOME TO THE MAGIC NUMBER GAME ****************");
+        Console.WriteLine();
+        Console.WriteLine("Guess a Number Between 1 - 100:");
+        Console.WriteLine();
+        // Console.Write("What is the magic number? ");
+        // string magicNumber = Console.ReadLine();
+        // int iMagicNumber = int.Parse(magicNumber);
+
+        Random numberGenerator = new Random();
+        int iMagicNumber = numberGenerator.Next(1, 101);
+
+        int iGuessNumber = -1;
+        while (iMagicNumber != iGuessNumber)
+        {
+            Console.Write("What is your guess? ");
+            string guessNumber = Console.ReadLine();
+            iGuessNumber = int.Parse(guessNumber);
+
+            if (iMagicNumber == iGuessNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("*** YOU GOT IT!!! ***");
+            }
+            else if(iMagicNumber < iGuessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+        }
     }
 }
