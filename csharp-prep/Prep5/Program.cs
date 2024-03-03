e912a31b356c19d0455d6c7422afb5d0d97e3adc
Yoav-Galindo <gal22005@byui.edu>
2024-03-02 23:11:38 -0700

    squarePrep5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        WelcomeMsg();
+        string uName = NameImput();
+        int uNumber = NumberImput();
+        int sNumber = ComputeSquare(uNumber);
+        NameAndSquare(uName, sNumber);
+
+        
     }
+    static void WelcomeMsg()
+    {
+        Console.WriteLine("*** Welcome to the program! ***");
+    }
+    static string NameImput()
+    {
+        Console.Write("Please enter your name: ");
+        string sName = Console.ReadLine();
+        return sName;
+    }
+    static int NumberImput()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string sNumber = Console.ReadLine();
+        int iNumber = int.Parse(sNumber);
+        return iNumber;
+    }
+    static int ComputeSquare(int iNumber)
+    {
+        int square = iNumber * iNumber;
+        return square;
+    }
+    static void NameAndSquare(string sName, int square)
+    {
+        Console.WriteLine($"*** {sName}, the square of your number is {square} ***");
+    }
+
 }
