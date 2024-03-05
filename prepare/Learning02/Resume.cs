e261a4c15ebb0627499a6338dd56a032970954fa
Yoav-Galindo <gal22005@byui.edu>
2024-03-05 08:53:18 -0700

    myResumeL02

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,20 @@
+public class Resume
+{
+    public string _name = "";
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        
+        Console.WriteLine();
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs: ");
+
+        foreach (Job jobIterate in _jobs)
+        {
+            jobIterate.Display();
+        }
+        Console.WriteLine();
+    }
+
+}
