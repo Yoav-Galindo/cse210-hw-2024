5799651b0f77541c94ce56f2fcdd26ec3020a193
Yoav-Galindo <gal22005@byui.edu>
2024-03-13 08:06:45 -0600

    fraction1

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fractionNoParameters = new Fraction();
+        Console.WriteLine(fractionNoParameters.GetFractionString());
+        Console.WriteLine(fractionNoParameters.GetDecimalValue());
+        
+        Fraction fraction1Parameter = new Fraction(5);
+        Console.WriteLine(fraction1Parameter.GetFractionString());
+        Console.WriteLine(fraction1Parameter.GetDecimalValue());
+        
+
+        Fraction fraction2Parameters = new Fraction(3,4);
+        Console.WriteLine(fraction2Parameters.GetFractionString());
+        Console.WriteLine(fraction2Parameters.GetDecimalValue());
+
+        Fraction f4 = new Fraction(1,3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
     }
+
 }
