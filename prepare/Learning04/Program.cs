ef1c6b227058041181d60d2a7bc88f8bedb14826
Yoav-Galindo <gal22005@byui.edu>
2024-03-21 07:34:14 -0600

    assignmentsProgram

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Roberto Galindo", "Programming");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Lorely Hernandez", "Fractions", "7.3", "3-10, 20-21");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Carlos Antonio", "European History", "The Causes of World War");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+
     }
 }
